8127f37f91d12b194e43497476ca95cca5588724
Alisson Leal <alissonsleal@hotmail.com>
2022-05-28 17:58:56 -0300

    chore: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,5 +31,19 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        brapi: {
+          primary: '#7347ab',
+          secondary: '#f6d860',
+          accent: '#37cdbe',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+      'dark',
+    ],
+  },
 };
