0d07512bf0ec81744c762f989d7eadbe67a63582
Alisson Leal <alissonsleal@hotmail.com>
2022-05-28 22:42:29 -0300

    fix: remove unnecessary mobile padding increase above fold content

--- components/Main/Hero.tsx
+++ components/Main/Hero.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export const Hero = () => {
   return (
     <section className="text-gray-400 bg-gray-900 body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-5 py-0 md:py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-white max-w-sm">
             Use a melhor API de finanças do Brasil
