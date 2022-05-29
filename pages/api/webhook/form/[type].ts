00d0618a44338f9442c0f04fecc428588950a156
Alisson Leal <alissonsleal@hotmail.com>
2022-05-29 16:00:43 -0300

    chore: correctly parse formData in webhook

--- pages/api/webhook/form/[type].ts
+++ pages/api/webhook/form/[type].ts
@@ -1,44 +1,50 @@
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+interface ILooseObject {
+  [key: string]: any;
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const data = req.body;
   const formType = req.query.type;
-  console.log({ data });
 
-  const formData = JSON.parse(data?.form_data || '{}');
-  console.log({ formData });
+  const formData = JSON.parse(data?.form_data || '{}') as ILooseObject;
 
-  // remove keys that start with _
   const cleanFormData = Object.keys(formData).reduce((acc, key) => {
+    // remove keys that start with _
     if (key.startsWith('_')) {
       return acc;
     }
+
     return {
       ...acc,
-      [key]: data[key],
+      [key]: formData[key as keyof typeof formData],
     };
-  }, {}) as { [key: string]: any };
+  }, {}) as ILooseObject;
 
-  console.log({ cleanFormData });
+  try {
+    await axios.post(`${process.env.DISCORD_WEBHOOK_URL}`, {
+      username: 'brapi',
+      avatar_url: 'https://brapi.ga/favicon.png',
+      embeds: [
+        {
+          title: `New ${formType} Form Submission`,
+          description: cleanFormData?.email || '',
+          color: 7419530,
+          fields: Object.entries(cleanFormData || {}).map(([key, value]) => ({
+            name: key,
+            value,
+          })),
+        },
+      ],
+    });
 
-  await axios.post(`${process.env.DISCORD_WEBHOOK_URL}`, {
-    username: 'brapi',
-    avatar_url: 'https://brapi.ga/favicon.png',
-    embeds: [
-      {
-        title: `New ${formType} Form Submission`,
-        description: cleanFormData?.email || '',
-        color: 7419530,
-        fields: Object.entries(cleanFormData || {}).map(([key, value]) => ({
-          name: key,
-          value,
-        })),
-      },
-    ],
-  });
+    console.log('webhook sent to discord');
 
-  console.log('webhook sent to discord');
-
-  res.status(200).json({ message: 'ok' });
+    res.status(200).json({ message: 'ok' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 };
