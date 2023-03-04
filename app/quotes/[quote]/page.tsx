38139947f08cfdce03260bab2d3efb22992b7b74
Alisson Leal <alissonsleal@hotmail.com>
2023-03-04 18:29:57 -0300

    chore: add ts-expect-error for server component

--- app/quotes/[quote]/page.tsx
+++ app/quotes/[quote]/page.tsx
@@ -5,6 +5,7 @@ const Quotes = () => {
     <main>
       <section className="max-w-screen min-h-screen">
         <div className="container mx-auto flex flex-col px-5 py-0 md:py-2 items-center">
+          {/* @ts-expect-error Server Component */}
           <MainQuotes />
         </div>
       </section>
