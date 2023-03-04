8c0193ac876b81616997ecdc8e9c345aeb98da33
Alisson Leal <alissonsleal@hotmail.com>
2023-03-04 17:02:50 -0300

    feat: fetch real data in page level

--- components/MainQuotes/index.tsx
+++ components/MainQuotes/index.tsx
@@ -1,7 +1,21 @@
 import { SideScroller } from '~/components/MainQuotes/SideScroller';
 
-const MainQuotes = () => {
-  return <SideScroller />;
+const getData = async () => {
+  const res = await fetch(
+    'https://brapi.dev/api/quote/list?sortBy=volume&limit=6',
+  );
+  const data = await res.json();
+  return data?.stocks;
+};
+
+const MainQuotes = async () => {
+  const quotes = await getData();
+
+  return (
+    <div className="py-4 flex w-full">
+      <SideScroller quotes={quotes} />
+    </div>
+  );
 };
 
 export default MainQuotes;
