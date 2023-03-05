7b3f478ba71adb2e064bbef7b1bf3c2b0c0c7a0e
Alisson Leal <alissonsleal@hotmail.com>
2023-03-04 21:53:12 -0300

    docs: add ts types to MainQuotes

--- components/MainQuotes/index.tsx
+++ components/MainQuotes/index.tsx
@@ -1,21 +1,41 @@
-import { SideScroller } from '~/components/MainQuotes/SideScroller';
+import { QuoteProps } from '~/@types/QuoteProps';
+import { MainQuote } from '~/components/MainQuotes/MainQuote';
 
-const getData = async () => {
+interface IMainQuotesProps {
+  currentStock: string;
+}
+
+interface IQuote extends QuoteProps {
+  historicalDataPrice: {
+    date: number;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+  };
+}
+
+const getCurrentQuote = async (stock: string) => {
   const res = await fetch(
-    'https://brapi.dev/api/quote/list?sortBy=volume&limit=20',
+    `https://brapi.dev/api/quote/${stock}?range=1y&interval=1d&fundamental=true`,
   );
   const data = await res.json();
-  return data?.stocks;
+  return (data?.results?.[0] || null) as IQuote;
 };
 
-const MainQuotes = async () => {
-  const quotes = await getData();
+const MainQuotes = async ({ currentStock }: IMainQuotesProps) => {
+  const currentQuote = await getCurrentQuote(currentStock);
+
+  if (!currentQuote) {
+    return <div>Não foi possível carregar os dados de {currentStock}</div>;
+  }
 
   return (
     <div className="py-4 flex w-full flex-col space-y-4">
-      <div className="space-y-2">
-        <span className="text-2xl font-bold">Ativos mais negociados hoje</span>
-        <SideScroller quotes={quotes} />
+      <div>
+        {/* @ts-expect-error Server Component */}
+        <MainQuote quote={currentQuote} />
       </div>
     </div>
   );
