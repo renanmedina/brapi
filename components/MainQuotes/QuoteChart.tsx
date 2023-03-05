55d95e8c48a24cc5ddd1b38f873f446d6aaed97e
Alisson Leal <alissonsleal@hotmail.com>
2023-03-05 02:03:29 -0300

    feat: use react-financial-charts instead of chart.js

--- components/MainQuotes/QuoteChart.tsx
+++ components/MainQuotes/QuoteChart.tsx
@@ -1,68 +1,132 @@
 'use client';
 
-import Chart from 'chart.js/auto';
-import { useEffect, useRef } from 'react';
+import { format } from 'd3-format';
+import { timeFormatLocale } from 'd3-time-format';
+
+import {
+  MouseCoordinateX,
+  MouseCoordinateY,
+  Chart,
+  ChartCanvas,
+  BarSeries,
+  XAxis,
+  YAxis,
+  discontinuousTimeScaleProviderBuilder,
+  AreaSeries,
+  CrossHairCursor,
+  OHLCTooltip,
+} from 'react-financial-charts';
 import { IHistoricalDataPrice } from '~/@types/IHistoricalDataPrice';
-import { numberToMoney } from '~/utils/formatNumbers';
 
 interface IQuoteChartProps {
   historicalDataPrices: IHistoricalDataPrice[];
 }
 
+const localeDefinition = timeFormatLocale({
+  dateTime: '%A, %e de %B de %Y. %X',
+  date: '%d/%m/%Y',
+  time: '%H:%M:%S',
+  periods: ['AM', 'PM'],
+  days: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  shortDays: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  months: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ],
+  shortMonths: [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
+  ],
+});
+
 export const QuoteChart = ({ historicalDataPrices }: IQuoteChartProps) => {
-  const chartRef = useRef(null);
+  const initialData = historicalDataPrices;
+  const height = 300;
+  const ratio = 1;
+  const width = 600;
+  const margin = { left: 0, right: 40, top: 24, bottom: 24 };
 
-  useEffect(() => {
-    const ctx = chartRef.current.getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: historicalDataPrices.map((data) =>
-          new Date(data.date * 1000).toLocaleDateString('pt-BR'),
-        ),
-        datasets: [
-          {
-            label: 'Preço',
-            data: historicalDataPrices.map((data) => data.close),
-            fill: true,
-            backgroundColor: '#9d7ec4',
-            borderColor: '#9d7ec4',
-            pointRadius: 0,
-            tension: 0.1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            ticks: {
-              callback: (value) => {
-                return numberToMoney(value);
-              },
-            },
-          },
-        },
-        responsive: true,
-        hover: {
-          mode: 'nearest',
-          intersect: false,
-          includeInvisible: true,
-        },
-        showLine: true,
-        maintainAspectRatio: false,
-        resizeDelay: 0,
-      },
-    });
+  const gridHeight = height - margin.top - margin.bottom;
 
-    return () => {
-      myChart.destroy();
-    };
-  }, [historicalDataPrices]);
+  const barChartHeight = gridHeight / 4;
+  const barChartOrigin = (_: number, h: number) => [0, h - barChartHeight];
+
+  const xScaleProvider = discontinuousTimeScaleProviderBuilder().inputDateAccessor(
+    (d) => new Date(d.date * 1000),
+  );
+
+  const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(
+    initialData,
+  );
+
+  const maxX = xAccessor(data[data.length - 1]);
+  const minX = xAccessor(data[Math.max(0, data.length - 100)]);
+  const xExtents = [minX, maxX];
+
+  const timeDisplayFormat = localeDefinition.format('%d %b %Y');
+
+  const pricesDisplayFormat = format('.2f');
 
   return (
-    <div className="flex flex-col flex-1">
-      <canvas ref={chartRef} />
-    </div>
+    <ChartCanvas
+      height={height}
+      ratio={ratio}
+      width={width}
+      margin={margin}
+      data={data}
+      displayXAccessor={displayXAccessor}
+      seriesName="Data"
+      xScale={xScale}
+      xAccessor={xAccessor}
+      xExtents={xExtents}
+    >
+      <Chart id={1} yExtents={(d) => [d.high, d.low]}>
+        <MouseCoordinateX displayFormat={timeDisplayFormat} />
+        <MouseCoordinateY
+          rectWidth={margin.right}
+          displayFormat={pricesDisplayFormat}
+        />
+        <AreaSeries yAccessor={(d) => d.close} />
+        <XAxis />
+        <YAxis />
+        <OHLCTooltip origin={[8, 16]} />
+      </Chart>
+      <Chart
+        id={2}
+        height={barChartHeight}
+        origin={barChartOrigin}
+        yExtents={(d) => d.volume}
+      >
+        <BarSeries
+          fillStyle={(d) =>
+            d.close > d.open
+              ? 'rgba(38, 160, 150, 0.3)'
+              : 'rgba(239, 80, 80, 0.3)'
+          }
+          yAccessor={(d) => d.volume}
+        />
+      </Chart>
+      <CrossHairCursor />
+    </ChartCanvas>
   );
 };
