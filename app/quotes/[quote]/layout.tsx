bb31efc101df4717b86ab61f3df78720791799de
Alisson Leal <alissonsleal@hotmail.com>
2023-03-04 21:52:16 -0300

    feat: create initial quotes quote layout page

---
+++ app/quotes/[quote]/layout.tsx
@@ -0,0 +1,29 @@
+import { SideScroller } from '~/components/MainQuotes/SideScroller';
+
+interface QuotesQuoteLayoutProps {
+  children: React.ReactNode;
+}
+
+const getData = async () => {
+  const res = await fetch(
+    'https://brapi.dev/api/quote/list?sortBy=volume&limit=20',
+  );
+  const data = await res.json();
+  return data?.stocks;
+};
+
+export default async function QuotesQuoteLayout({
+  children,
+}: QuotesQuoteLayoutProps) {
+  const quotes = await getData();
+
+  return (
+    <>
+      <div className="space-y-2 container mx-auto px-5 pt-6">
+        <span className="text-2xl font-bold">Ativos mais negociados hoje</span>
+        <SideScroller quotes={quotes} />
+      </div>
+      {children}
+    </>
+  );
+}
