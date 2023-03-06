6fb8e3eeb2f195a98bf234cc15ba23a97a308a6b
Alisson Leal <alissonsleal@hotmail.com>
2023-03-06 00:26:27 -0300

    feat: add 52 week min-max data in quotes

--- components/MainQuotes/MainQuote.tsx
+++ components/MainQuotes/MainQuote.tsx
@@ -12,11 +12,20 @@ interface IMainQuoteProps {
 export const MainQuote = async ({ quote }: IMainQuoteProps) => {
   return (
     <div className="flex space-y-6 max-w-full flex-col">
-      <div className="flex space-x-2 items-center">
-        <img src={quote.logourl} className="w-12 h-12 rounded-md" />
-        <div className="flex flex-col">
-          <span className="text-2xl font-bold">{quote.symbol}</span>
-          <span className="text-sm">{quote.longName}</span>
+      <div className="flex justify-between">
+        <div className="flex space-x-2 items-center">
+          <img src={quote.logourl} className="w-12 h-12 rounded-md" />
+          <div className="flex flex-col">
+            <span className="text-2xl font-bold">{quote.symbol}</span>
+            <span className="text-sm">{quote.longName}</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-end">
+          <span className="text-sm w-fit">Última atualização</span>
+          <span className="text-sm font-bold w-fit">
+            {new Date(quote.regularMarketTime).toLocaleString()}
+          </span>
         </div>
       </div>
 
@@ -27,6 +36,7 @@ export const MainQuote = async ({ quote }: IMainQuoteProps) => {
             {numberToMoney(quote.regularMarketPrice)}
           </span>
         </div>
+
         <div className="flex flex-col">
           <span className="text-sm">Variação (dia)</span>
           <span className="text-2xl font-bold">
@@ -35,12 +45,21 @@ export const MainQuote = async ({ quote }: IMainQuoteProps) => {
             {quote.regularMarketChangePercent > 0 ? '▲' : '▼'}
           </span>
         </div>
+
         <div className="flex flex-col">
-          <span className="text-sm">Última atualização</span>
+          <span className="text-sm">Min. 52 Semanas</span>
           <span className="text-2xl font-bold">
-            {new Date(quote.regularMarketTime).toLocaleString()}
+            {numberToMoney(quote.fiftyTwoWeekLow)}
           </span>
         </div>
+
+        <div className="flex flex-col">
+          <span className="text-sm">Máx. 52 Semanas</span>
+          <span className="text-2xl font-bold">
+            {numberToMoney(quote.fiftyTwoWeekHigh)}
+          </span>
+        </div>
+
         <div className="flex flex-col">
           <span className="text-sm">Capitalização de mercado</span>
           <span className="text-2xl font-bold">
